6a11e8ac3d76f81fb08d12e98caa62b2480f8ea2
baopham <pqbao27@gmail.com>
2022-09-01 18:32:48 +0700

    cm

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { Layout } from '@/components/Layouts'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Layout />
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
